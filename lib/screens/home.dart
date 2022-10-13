3450fcfa622b678f33947996bab680b590590d0b
tauqeerkhattak <17sw72@students.muet.edu.pk>
2022-10-13 16:28:34 +0500

    removed ios build

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -26,7 +26,7 @@ class _HomeState extends State<Home> {
               await BackgroundService.start();
             },
             child: const Text(
-              'SHOW NOTIFICATION',
+              'DISPLAY NOTIFICATION',
             ),
           ),
         ],
